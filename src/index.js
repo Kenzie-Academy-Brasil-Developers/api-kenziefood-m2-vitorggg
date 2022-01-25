e54ff28b224538f3e2910d946f0b2d975765c779
Omar <omarucolombari@gmail.com>
2022-01-25 19:18:16 -0300

    Finished function add to cart

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import { cart } from "./modules/cart";
+import { cart } from "./modules/cart.js";
+
+const products = document.getElementsByClassName('productsShowcase-list');
+
+products[0].addEventListener('click', (e) => {
+    let product = e.target;
+    if(product.className === 'product-cart'){
+        console.log(product)
+        cart.add('cart', product)
+    }
+})
--- src/modules/cart.js
+++ src/modules/cart.js
@@ -3,8 +3,19 @@ const cart = class cart {
     static
     add(idCart, product) {
         const cart = document.getElementById(idCart)
-        const clone = product.parentNode.cloneNode(true)
-        cart.appendChild(clone)
+        const products = document.createElement('ul')
+        const clone = product.parentNode.parentNode.parentNode.cloneNode(true)
+        const remove = document.createElement('img')
+        remove.src = 'public/icons/Group 1033.png'
+        remove.className = 'remove-product'
+
+
+        clone.className = 'products-cart'
+        cart.className = 'with-products'
+        console.log(clone)
+        clone.appendChild(remove)
+        products.appendChild(clone)
+        cart.appendChild(products)
     }
 }
 
